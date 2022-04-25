67b7b7c962c0ccb7035d43256c1e218cc3f348f2
Stoyan Grozdanov <st.grozdanov@abv.bg>
2022-04-25 11:11:26 +0300

    implemented header design

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ export default function Header() {
             <header>
                 <Navigation />
                 <article>
-                    <h2>THE RELIABLE <span>DEVELOPER</span> FOR <br />YOUR PROJECTS</h2>
+                    <h2>THE RELIABLE <br /><span>DEVELOPER</span> <br />FOR YOUR PROJECTS</h2>
                     <h3>WELCOME TO MY PORTFOLIO</h3>
                     <p>Together we build the foundations of tommorow</p>
                 </article>
--- src/components/Header/HeaderNavigation.js
+++ src/components/Header/HeaderNavigation.js
@@ -2,21 +2,21 @@ import './HeaderNavigation.css';
 
 export default function Navigation() {
     return (
-        <nav>
-            <ul className='nav-ul'>
-                <li>
-                    <a href="#">Introduction</a>
-                </li>
-                <li>
-                    <a href="#">About Me</a>
-                </li>
-                <li>
-                    <a href="#">My Projects</a>
-                </li>
-                <li>
-                    <a href="#">Contacts</a>
-                </li>
-            </ul>
-        </nav>
+            <nav>
+                <ul className='nav-ul'>
+                    <li>
+                        <a className='active' href="#">Introduction</a>
+                    </li>
+                    <li>
+                        <a href="#">About Me</a>
+                    </li>
+                    <li>
+                        <a href="#">My Projects</a>
+                    </li>
+                    <li>
+                        <a href="#">Contacts</a>
+                    </li>
+                </ul>
+            </nav>
     );
 }
