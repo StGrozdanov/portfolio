624889c59f6bf8eafcfea7d69608c78676c02e29
Stoyan Grozdanov <st.grozdanov@abv.bg>
2022-04-25 11:50:54 +0300

    implemented custom scrollbar and new header animation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,6 +10,7 @@ export default function Header() {
                     <h2 className='bounceIn'>THE RELIABLE <br /><span>DEVELOPER</span> <br />FOR YOUR PROJECTS</h2>
                     <h3 className='bounceIn'>WELCOME TO MY PORTFOLIO</h3>
                     <p className='bounceIn'>Together we build the foundations of tommorow</p>
+                    <i class="fa-solid fa-angles-down fa-beat-fade"></i>
                 </article>
             </header>
         </>
