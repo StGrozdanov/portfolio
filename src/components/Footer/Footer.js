c8ae04dc4d9c6f2921761daa5dc58912877ab9da
Stoyan Grozdanov <st.grozdanov@abv.bg>
2022-04-27 21:46:17 +0300

    design improvements

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,10 +5,26 @@ export default function Footer() {
         <footer className="site-footer">
             <article className='site-footer-left-article'>
                 <ul className="social-links">
-                    <li><a className="fadeInUp" href="#" style={{ animationDelay: 0.2 + 's' }}><i className="fa fa-facebook"></i></a></li>
-                    <li><a className="fadeInUp" href="https://www.linkedin.com/in/stoyan-grozdanov-533b4b1bb" target="blank" style={{ animationDelay: 0.3 + 's' }}><i className="fa fa-linkedin"></i></a></li>
-                    <li><a className="fadeInUp" href="https://github.com/StGrozdanov" target="blank" style={{ animationDelay: 0.4 + 's' }}><i className="fa fa-github"></i></a></li>
-                    <li><a className="fadeInUp" href="mailto:st.grozdanov@abv.bg" style={{ animationDelay: 0.5 + 's' }}><i className="fa fa-envelope"></i></a></li>
+                    <li className='facebook'>
+                        <a className="fadeInUp" href="#" style={{ animationDelay: 0.2 + 's' }}>
+                            <i className="fa fa-facebook"></i>
+                        </a>
+                    </li>
+                    <li className='linkedIn'>
+                        <a className="fadeInUp" href="https://www.linkedin.com/in/stoyan-grozdanov-533b4b1bb" target="blank" style={{ animationDelay: 0.3 + 's' }}>
+                            <i className="fa fa-linkedin"></i>
+                        </a>
+                    </li>
+                    <li className='github'>
+                        <a className="fadeInUp" href="https://github.com/StGrozdanov" target="blank" style={{ animationDelay: 0.4 + 's' }}>
+                            <i className="fa fa-github"></i>
+                        </a>
+                    </li>
+                    <li className='email'>
+                        <a className="fadeInUp" href="mailto:st.grozdanov@abv.bg" style={{ animationDelay: 0.5 + 's' }}>
+                            <i className="fa fa-envelope"></i>
+                        </a>
+                    </li>
                 </ul>
             </article>
             <article className="bounceIn site-footer-right-article" style={{ animationDelay: 0.2 + 's', animationDuration: 0.75 + 's' }}>
