6a5a40ab53a8dc09aae4b950efe22d2e8a5ae8a3
Stoyan Grozdanov <st.grozdanov@abv.bg>
2022-04-27 19:27:20 +0300

    Created website footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import AboutMe from "./components/AboutMe/AboutMe";
 import AboutMeSkills from "./components/AboutMe/AboutMeSkills";
 import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header"
 import Projects from "./components/Projects/Projects";
 
@@ -12,6 +13,7 @@ function App() {
       <AboutMeSkills />
       <Projects />
       <Contact />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,25 @@
+import './Footer.css';
+
+export default function Footer() {
+    return (
+        <footer className="site-footer">
+            <article className='site-footer-left-article'>
+                <ul className="social-links">
+                    <li><a className="fadeInUp" href="#" style={{ animationDelay: 0.2 + 's' }}><i className="fa fa-facebook"></i></a></li>
+                    <li><a className="fadeInUp" href="https://www.linkedin.com/in/stoyan-grozdanov-533b4b1bb" target="blank" style={{ animationDelay: 0.3 + 's' }}><i className="fa fa-linkedin"></i></a></li>
+                    <li><a className="fadeInUp" href="https://github.com/StGrozdanov" target="blank" style={{ animationDelay: 0.4 + 's' }}><i className="fa fa-github"></i></a></li>
+                    <li><a className="fadeInUp" href="mailto:st.grozdanov@abv.bg" style={{ animationDelay: 0.5 + 's' }}><i className="fa fa-envelope"></i></a></li>
+                </ul>
+            </article>
+            <article className="bounceIn site-footer-right-article" style={{ animationDelay: 0.2 + 's', animationDuration: 0.75 + 's' }}>
+                <h3 className="footer-title">Stoyan Grozdanov</h3>
+                <ul className='site-footer-life-rules'>
+                    <li>Live</li>
+                    <li>Learn</li>
+                    <li>Progress</li>
+                    <li>One step at a time.</li>
+                </ul>
+            </article>
+        </footer >
+    );
+}
