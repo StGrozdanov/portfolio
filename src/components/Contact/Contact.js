7e07236276625bf6f3590ca194e49418add68daa
Stoyan Grozdanov <st.grozdanov@abv.bg>
2022-04-28 22:50:18 +0300

    contact form is now working and sends emails

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,36 @@
 import './Contact.css';
 import { AnimationOnScroll } from 'react-animation-on-scroll';
+import emailjs from 'emailjs-com';
+import { useRef, useState } from 'react';
+
+const thankYouMessage = (
+    <h4 className='contact-section-send-success'>
+        Your message was delivered! Thanks for keeping in touch with me.
+    </h4>
+);
 
 export default function Contact() {
+    const [sendEmail, setSendEmail] = useState(false);
+    const form = useRef();
+
+    function sendEmailHandler(e) {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        let { name, email, description } = Object.fromEntries(formData);
+
+        if (email.trim() !== '' && name.trim() !== '' && description.trim() !== '') {
+            emailjs
+                .sendForm('service_oqn9vj9', 'template_igd22ij', form.current, 'n0npgfKwhAdyT8_tv')
+                .then(result => {
+                    if (result.text == 'OK') {
+                        setSendEmail(true);
+                        e.target.reset();
+                    }
+                });
+        }
+    }
+
     return (
         <section id='contacts' className='contact-section'>
             <header className="contact-section-header">
@@ -9,16 +38,17 @@ export default function Contact() {
                 <p>Don't hesitate to contact me, together we are stronger</p>
             </header>
             <main>
-                <form action="#" method="post">
+                <form ref={form} onSubmit={sendEmailHandler}>
+                    {sendEmail ? thankYouMessage : null}
                     <AnimationOnScroll animateIn='fadeInUp'>
                         <article className='contact-section-credentials-article'>
-                            <input type="text" placeholder="Name" name="name" id="name" required="" />
-                            <input type="email" placeholder="E-mail" name="email" id="email" pattern="^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$" required="" />
+                            <input type="text" placeholder="Name" name="name" id="name" required={true} />
+                            <input type="email" placeholder="E-mail" name="email" id="email" pattern="^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$" required={true} />
                         </article>
                     </AnimationOnScroll>
                     <AnimationOnScroll animateIn='fadeInUp' delay={100}>
                         <article>
-                            <textarea placeholder="Message" rows="7" name="description" id="description" required=""></textarea>
+                            <textarea placeholder="Message" rows="7" name="description" id="description" required={true}></textarea>
                         </article>
                     </AnimationOnScroll>
                     <AnimationOnScroll animateIn='fadeInUp' delay={500}>
