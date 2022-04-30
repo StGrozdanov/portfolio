9d0600c81ddda75736d97f5bc225f7e9ff111e52
Stoyan Grozdanov <st.grozdanov@abv.bg>
2022-04-30 10:48:13 +0300

    Improved responsive design

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -9,14 +9,14 @@ export default function AboutMe() {
                     <h2 className='about-me-article-heading'>About Me</h2>
                     <p>I am a full-stack developer, mostly experienced in Spring and React</p>
                     <p className='about-me-article-story'>
-                        For me, building things from scratch is an enjoyment and bringing ideas to life is a lifestyle.
+                        For me building things from scratch is an enjoyment and bringing ideas to life is a lifestyle.
                         In the past I tried many other career paths - from road construction engineering, to pursuing sports
                         career. Yet again, after the start of COVID-19 pandemic, I found myself on a new path - a path in
                         which my passion and my enjoyment are always the leading part of the process.
                     </p>
                 </article>
             </AnimationOnScroll>
-            <AnimationOnScroll animateIn='fadeInRight'>
+            <AnimationOnScroll animateIn='fadeInRight about-me-animation-container'>
                 <div className='about-me-picture-container'>
                     <img className="about-img" src="assets/images/coding-science.jpg" />
                 </div>
